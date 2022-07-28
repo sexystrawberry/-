a747c533a77babefe39e5570069a2f81e36e9e39
tj-bu <tj-bu@DESKTOP-03EO753>
2022-07-28 16:59:44 +0900

    두번째★

--- gitTest/src/gitTest/gitTest.java
+++ gitTest/src/gitTest/gitTest.java
@@ -5,7 +5,6 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("HELLO JAVA");
 		
 		int i;
 		int j;
@@ -13,7 +12,7 @@ public class gitTest {
 		for(i=1; i<=9; i++) {
 			System.out.println(i + "´Ü");
 			for(j=1; j<=9; j++) {
-				System.out.printf("%d x %d = %d /n", i, j, i*j);
+				System.out.printf("%d x %d = %d /", i, j, i*j);
 			}
 		}
 		
